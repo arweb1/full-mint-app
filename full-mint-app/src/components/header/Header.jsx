4076da06f7c4049b21de5b9458a3ae0d7c3f5a6b
arweb1 <agaevrweb@gmail.com>
2024-02-28 00:58:23 +0200

    Wallet component updated

--- full-mint-app/src/components/header/Header.jsx
+++ full-mint-app/src/components/header/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-
+import ConnectWallet from '../../features/wallet-components/ConnectWallet';
+import './Header.scss';
 
 function Header() {
   return (
-    <div>
-        <h1>Logo</h1>
-        
+    <div className='header'>
+      <div className="left-side">
+        <img src="https://xter.io/images/xterio_colorful.svg?w=200&q=80" alt="Logo" />
+        <ul>
+          <li>Games</li>
+          <li>Launchpad</li>
+        </ul>
+      </div>
+      <div className="right-side">
+          <ConnectWallet/>
+      </div>
     </div>
   )
 }
--- full-mint-app/src/features/wallet-components/ConnectWallet.jsx
+++ full-mint-app/src/features/wallet-components/ConnectWallet.jsx
@@ -1,19 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect, useDispatch, useSelector } from 'react-redux';
 import { connectWallet, updateBalance } from '../walletThunks';
-import { setAddress, setConnected } from '../walletSlice';
+import { setAddress, setBalance, setConnected } from '../walletSlice';
 import { ethers } from 'ethers';
+
+import './ConnectWallet.scss';
+
+import copyImage from '../../storage/images/copy-svgrepo-com.svg';
 
 function ConnectWallet() {
     const dispatch = useDispatch();
     const walletAddress = useSelector(state => state.wallet.address ? state.wallet.address : null);
-    const isConnected = useSelector(state => state.wallet.isConnected)
-    const walletBalance = useSelector(state => state.wallet.balance)
-    const select = useSelector(state => state)
-    console.log(select)
+    const isConnected = useSelector(state => state.wallet.isConnected);
+    const walletBalance = useSelector(state => state.wallet.balance);
 
+    const [isWalletOptionsOpen, setIsWalletOptionsOpen] = useState(false);
+
+    //Wallet connect
     const handleWalletConnect = () => {
-        console.log('click')
         dispatch(connectWallet())
     }
     //Addreess wallet update
@@ -31,7 +35,7 @@ function ConnectWallet() {
     //Balance update real time
     useEffect(() => {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
-    
+
         const handleAccountsChanged = accounts => {
             if (accounts.length > 0) {
                 const userAddress = accounts[0];
@@ -44,34 +48,107 @@ function ConnectWallet() {
                 dispatch(setConnected(false));
             }
         };
-    
+
         window.ethereum.on('accountsChanged', handleAccountsChanged);
-    
+
         return () => {
             window.ethereum.removeListener('accountsChanged', handleAccountsChanged);
         };
     }, [dispatch]);
-    
+
+    //Open Wallet options
+
+    const toggleWalletOptionsOpen = () => {
+        setIsWalletOptionsOpen(!isWalletOptionsOpen);
+    }
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            const walletOptionsElement = document.querySelector('.walletOptions');
+            const walletInfo = document.querySelector('.walletInfo');
+            const copyWallet = document.querySelector('.copyWallet')
+            console.log(event.target)
+            if (walletOptionsElement && !walletOptionsElement.contains(event.target) && !walletInfo.contains(event.target) && !copyWallet.contains(event.target)) {
+                setIsWalletOptionsOpen(false);
+            }
+        };
+
+        if (isWalletOptionsOpen) {
+            window.addEventListener('click', handleClickOutside);
+        }
+
+        return () => {
+            window.removeEventListener('click', handleClickOutside);
+        };
+    }, [isWalletOptionsOpen]);
+
+
     //Wallet disconnect update
 
     //Wallet Mask
-
     const handleWalletMask = wallet => {
         const visibleCharacters = 6;
         const maskedAddress = `${wallet.slice(0, visibleCharacters)}...${wallet.slice(-4)}`;
         return maskedAddress
     }
-    
-    
+
+    //disconnect 
+    const handleDisconnectWallet = async () => {
+        try {
+            if (window.ethereum) {
+                await window.ethereum.request({ method: 'eth_requestAccounts', params: [] })
+                dispatch(setAddress(null))
+                dispatch(setBalance(null))
+                dispatch(setConnected(false))
+            } else {
+                console.log('Something wrong with MetaMask')
+            }
+        } catch (error) {
+            console.log('Error: ', error)
+        }
+    }
+
+    //Copy on Click
+    const handleWalletCopy = () => {
+        if (walletAddress) {
+            navigator.clipboard.writeText(walletAddress)
+                .then(() => {
+                    console.log('Copied')
+                })
+                .catch(error => {
+                    console.log('Error: ', error)
+                })
+        }
+    }
+
     return (
         <div>
             {isConnected ? (
-                <div>
-                    <p>Connected Address: {walletAddress ? handleWalletMask(walletAddress) : "Can't load"}</p>
-                    <p>Balance: {walletBalance ? parseFloat(walletBalance).toFixed(2) : "Loading..."}</p>
+                <div className='connectedWallet' >
+                    <div className="walletInfo" onClick={toggleWalletOptionsOpen}>
+                        <div className="wallet">
+                            <p>{walletAddress ? handleWalletMask(walletAddress) : "Can't load"}</p>
+                            <img
+                                src={copyImage}
+                                alt="Copy"
+                                className='copyWallet'
+                                onClick={e => {
+                                    handleWalletCopy();
+                                    e.stopPropagation();
+                                }} />
+                        </div>
+                        <p>Balance: {walletBalance ? parseFloat(walletBalance).toFixed(2) : "Loading..."}E</p>
+                    </div>
+                    {isWalletOptionsOpen && (
+                        <div className="walletOptions">
+                            <ul>
+                                <li onClick={handleDisconnectWallet}>Disconnect</li>
+                            </ul>
+                        </div>
+                    )}
                 </div>
             ) : (
-                <button onClick={handleWalletConnect}>Connect Wallet </button>
+                <button className='connectBtn' onClick={handleWalletConnect}>Connect Wallet </button>
             )}
         </div>
     )
